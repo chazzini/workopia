fc8137d69ce3b6dd8e420a880867d153d6d0b8b9
Chinedu Chaz <ccognate@gmail.com>
2024-03-31 13:15:53 +0100

    feat: create partials & include helper

--- helper.php
+++ helper.php
@@ -5,8 +5,43 @@
  * @param string $path
  * @return string
  */
-
- function  basePath($path ='')
+ function  basePath(string $path =''):string
  {
     return __DIR__.'/'.$path;
  }
+
+ /**
+  * Load view
+  *
+  * @param string $view
+  * @return void
+  */
+  function loadView(string $view):void
+  {
+    $viewPath =basePath("views/{$view}.view.php");
+
+    if(file_exists($viewPath)){
+
+        require $viewPath;
+    }else{
+        echo "{$view} not found";
+    }
+  }
+ /**
+  * Load partials
+  *
+  * @param string $view
+  * @return void
+  */
+  function loadPartial(string $view):void
+  {
+    
+    $viewPath = basePath("views/partials/{$view}.php");
+
+    if(file_exists($viewPath)){
+
+        require $viewPath;
+    }else{
+        echo "{$view} not found";
+    }
+  }
