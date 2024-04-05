e85953d98f1e18fe40cbe8859abc84ca178e4f7b
Chinedu Chaz <ccognate@gmail.com>
2024-04-05 16:26:29 +0100

    modified route to allow params

--- helper.php
+++ helper.php
@@ -1,87 +1,88 @@
 <?php
-/**
- * Get the base path
- * 
- * @param string $path
- * @return string
- */
-function basePath(string $path = ''): string
-{
-  return __DIR__ . '/' . $path;
-}
 
-/**
- * Load view
- *
- * @param string $view
- * @param array $data
- * @return void
- */
-function loadView(string $view, array $data = []): void
-{
-  $viewPath = basePath("App/views/{$view}.view.php");
-
-  if (file_exists($viewPath)) {
-    extract($data);
-    require $viewPath;
-  } else {
-    echo "{$view} not found";
+  /**
+   * Get the base path
+   * 
+   * @param string $path
+   * @return string
+   */
+ function basePath(string $path = ''): string
+  {
+    return __DIR__ . '/' . $path;
   }
-}
-/**
- * Load partials
- *
- * @param string $view
- * @return void
- */
-function loadPartial(string $view): void
-{
 
-  $viewPath = basePath("App/views/partials/{$view}.php");
+  /**
+   * Load view
+   *
+   * @param string $view
+   * @param array $data
+   * @return void
+   */
+ function loadView(string $view, array $data = []): void
+  {
+    $viewPath = basePath("App/views/{$view}.view.php");
 
-
-
-  if (file_exists($viewPath)) {
-
-    require $viewPath;
-  } else {
-    echo "{$view} not found";
+    if (file_exists($viewPath)) {
+      extract($data);
+      require $viewPath;
+    } else {
+      echo "{$view} not found";
+    }
   }
-}
+  /**
+   * Load partials
+   *
+   * @param string $view
+   * @return void
+   */
+ function loadPartial(string $view): void
+  {
 
-/**
- * Inspect  a value
- * 
- * @param mixed $value
- * @return void
- */
-function inspect(mixed $value): void
-{
-  echo '<pre>';
-  var_dump($value);
-  echo '</pre>';
-
-}
-/**
- * Inspect  a value and Die
- * 
- * @param mixed $value
- * @return void
- */
-function inspectAndDie(mixed $value): void
-{
-  echo '<pre>';
-  var_dump($value);
-  echo '</pre>';
-
-  die();
-
-}
+    $viewPath = basePath("App/views/partials/{$view}.php");
 
 
 
-function formatSalary(string $salary): string
-{
-  $formated_salary = '$' . number_format(floatval($salary));
-  return $formated_salary;
-}
+    if (file_exists($viewPath)) {
+
+      require $viewPath;
+    } else {
+      echo "{$view} not found";
+    }
+  }
+
+  /**
+   * Inspect  a value
+   * 
+   * @param mixed $value
+   * @return void
+   */
+ function inspect(mixed $value): void
+  {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+
+  }
+  /**
+   * Inspect  a value and Die
+   * 
+   * @param mixed $value
+   * @return void
+   */
+ function inspectAndDie(mixed $value): void
+  {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+
+    die();
+
+  }
+
+
+
+ function formatSalary(string $salary): string
+  {
+    $formated_salary = '$' . number_format(floatval($salary));
+    return $formated_salary;
+  }
